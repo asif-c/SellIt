719716602c3785ea36cc837de8516a51e40beca4
Clinton Bestmann <SESA514409@schneider-electric.com>
2019-01-22 15:11:09 -0800

    added post, and modified layout

---
+++ SellIt/wwwroot/js/layout.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", main);
+
+function main(ev) {
+
+    const navBar = document.getElementById('side-nav');
+    const navControl = document.getElementById('nav-control');
+    const mainContent = document.getElementById('main');
+
+    let navbarOpen = false;
+
+    navControl.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        const navBarWidth = navBar.clientWidth;
+        navBar.classList.toggle('active-nav');
+        navbarOpen = !navbarOpen;
+        mainContent.style.marginLeft = "0px";
+        if (navbarOpen) {
+            navControl.textContent = '<';
+            mainContent.style.marginLeft = navBarWidth + 'px';
+        } else {
+            navControl.textContent = '>';
+        }
+    });
+
+}
